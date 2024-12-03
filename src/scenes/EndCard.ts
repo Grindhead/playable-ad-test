7c57e91e71b02640effa109f754ff6d3e2a68f23
craig beswetherick <craigbeswetherick@gmail.com>
2024-12-03 02:31:54 +0000

    refactored end card into correct scene

--- src/scenes/EndCard.ts
+++ src/scenes/EndCard.ts
@@ -1,4 +1,6 @@
 import { Scene } from "phaser";
+import { config } from "../config";
+import { networkPlugin } from "../networkPlugin";
 
 export class EndCard extends Scene {
   constructor() {
@@ -6,19 +8,32 @@ export class EndCard extends Scene {
   }
 
   create() {
-    this.add
-      .text(512, 460, "Main Menu", {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 8,
-        align: "center",
-      })
-      .setOrigin(0.5);
+    const endCardSprite = this.add.sprite(
+      this.cameras.main.centerX,
+      0,
+      "atlas",
+      "logo.png"
+    );
 
+    endCardSprite.setOrigin(0.5, 1);
+    const scaleX = config.width / 3 / endCardSprite.width;
+    endCardSprite.setScale(scaleX);
+    endCardSprite.y = 200;
+
+    this.tweens.add({
+      targets: endCardSprite,
+      scale: endCardSprite.scale * 1.2,
+      duration: 1000,
+      ease: "Power2",
+      yoyo: true,
+      repeat: -1,
+    });
+
+    endCardSprite.setInteractive();
     this.input.once("pointerdown", () => {
-      this.scene.start("Game");
+      networkPlugin.ctaPressed(
+        "https://apps.apple.com/app/clean-manager/id123456789"
+      );
     });
   }
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,5 +1,4 @@
 import { config } from "../config";
-import { networkPlugin } from "../networkPlugin";
 
 export class MainScene extends Phaser.Scene {
   private currentImageIndex: number = 0;
@@ -114,32 +113,6 @@ export class MainScene extends Phaser.Scene {
     this.images.forEach((image) => image.destroy());
     this.images = [];
 
-    const endCardSprite = this.add.sprite(
-      this.cameras.main.centerX,
-      0,
-      "atlas",
-      "logo.png"
-    );
-
-    endCardSprite.setOrigin(0.5, 1);
-    const scaleX = config.width / 3 / endCardSprite.width;
-    endCardSprite.setScale(scaleX);
-    endCardSprite.y = 200;
-
-    this.tweens.add({
-      targets: endCardSprite,
-      scale: endCardSprite.scale * 1.2,
-      duration: 1000,
-      ease: "Power2",
-      yoyo: true,
-      repeat: -1,
-    });
-
-    endCardSprite.setInteractive();
-    this.input.once("pointerdown", () => {
-      networkPlugin.ctaPressed(
-        "https://apps.apple.com/app/clean-manager/id123456789"
-      );
-    });
+    this.scene.start("EndCard");
   }
 }
